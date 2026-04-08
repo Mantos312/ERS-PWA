7cc09198dd844cff8f2a39e5c2c9ef665da3e4ea
Mantos312 <136915863+Mantos312@users.noreply.github.com>
2026-04-08 15:37:00 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,37 @@
-const CACHE_NAME = 'ers-v1';
-const urlsToCache = ['/'];
+const CACHE_NAME = "ersiman-pwa-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon.png"
+];
 
-self.addEventListener('install', event => {
+// Install
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
